1c34ee84fef21810a5a937945e216ec02123a9cb
PorcoRosso85 <1.is.universe@gmail.com>
2023-11-21 07:17:50 +0900

    will fix why table not found

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -3,10 +3,10 @@ import Database from "better-sqlite3";
 
 import * as schema from "./schema";
 
-const sqlite = new Database("sqlite3.db");
-const db: BetterSQLite3Database<typeof schema> = drizzle(sqlite, { schema });
+const sqlite = new Database("./sqlite3.db");
+console.log(sqlite);
+const sqliteDatabase: BetterSQLite3Database<typeof schema> = drizzle(sqlite, {
+  schema,
+});
 
-await db.insert(schema.users).values({ name: "Por" });
-
-const result = await db.select().from(schema.users);
-console.log("result: ", result);
+export { sqliteDatabase };
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,5 +2,14 @@ import { databaseConfig } from "./config";
 import { databaseHonoApp } from "./route";
 import { insertEmailSchema } from "./validator";
 import { TestQuery } from "./TestQuery";
+import { sqliteDatabase } from "./client";
+import * as databaseSchema from "./schema";
 
-export { insertEmailSchema, databaseConfig, databaseHonoApp, TestQuery };
+export {
+  insertEmailSchema,
+  databaseConfig,
+  databaseHonoApp,
+  TestQuery,
+  sqliteDatabase,
+  databaseSchema,
+};
