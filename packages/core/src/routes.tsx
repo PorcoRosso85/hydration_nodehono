5ac722911c3958b306661b061105520b08a9de15
PorcoRosso85 <1.is.universe@gmail.com>
2023-11-29 08:49:41 +0900

    refactor core

--- packages/core/src/routes.tsx
+++ packages/core/src/routes.tsx
@@ -1,34 +1,37 @@
-import { Hono } from "hono";
-import type { FC } from "hono/jsx";
-import { html } from "hono/html";
+import { Hono } from 'hono'
+import { html } from 'hono/html'
+import type { FC } from 'hono/jsx'
 
-import { Meta } from "./components/Meta";
+import { Meta } from './components/Meta'
 
 const Add: FC = () => {
   // お悩みポイント 2
   return html`<div x-data="{add: function(x, y) { return x + y }}">
     <button type="button" x-on:click="alert(add(1, 2))">Show</button>
-  </div>`;
-};
+  </div>`
+}
 
-const app = new Hono();
+const app = new Hono()
 
 app
-  .use("*", async (c, next) => {
+  .use('*', async (c, next) => {
     // TODO: 指定のルートにhydrationする実装追加
     c.setRenderer((content) => {
-      return c.html(<Meta>{content}</Meta>);
-    });
-    await next();
+      return c.html(<Meta>{content}</Meta>)
+    })
+    await next()
   })
 
-  .get("/", (c) => {
+  .get('/', (c) => {
     return c.render(
       <>
         {/* <div hx-get="/about" hx-trigger="load, "></div> */}
         <div hx-get="/about/articles" hx-trigger="load, "></div>
-      </>
-    );
-  });
+      </>,
+    )
+  })
 
-export { app as coreHonoApp };
+export const coreHonoApp = {
+  endpoint: '/',
+  app: app,
+}
